b8939e9509e22592d3f8354d6909c81455d9ce8a
John Bartucz <jobartucz@isd535.org>
2021-12-03 15:24:18 -0600

    fixed clock

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2022 0:0:0").getTime();
+var countDownDate = new Date("Jan 1, 2022 0:0:01").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -11,7 +11,7 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24)) - 1;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
