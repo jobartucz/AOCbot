7a7c8997a6255f1a81779ef238a5d0c269d8fad2
Mr. Bartucz <jobartucz@isd535.org>
2021-12-03 13:29:06 -0600

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 1, 2022 0:0:0").getTime();
 
+alert("asdf");
+
 // Update the count down every 1 second
 var x = setInterval(function() {
 
