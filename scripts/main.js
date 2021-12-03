afca8e54618a5b94838c29171afa9e1112286e73
John Bartucz <jobartucz@isd535.org>
2021-12-03 15:08:29 -0600

    fixed countdown

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 1, 2022 0:0:0").getTime();
 
-console.log("hello?")
-
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -19,7 +17,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  document.getElementById("clock").innerHTML = "Time left: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
