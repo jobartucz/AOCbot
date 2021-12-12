ac98b302f400b54419faa571b6fe5e45102ecfcc
Junhao Zhang <junhaodev@gmail.com>
2021-12-11 20:45:43 -0600

    Fixed bug where countdown was not instant

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,31 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2022 0:0:01").getTime();
+let countDownDate = new Date("Jan 1, 2022 0:0:01").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+function countdown() {
 
   // Get today's date and time
-  var now = new Date().getTime();
-    
+  let now = new Date().getTime();
+
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
+  let distance = countDownDate - now;
+
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
   // Output the result in an element with id="clock"
   document.getElementById("clock").innerHTML = "Time left: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    
+
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("clock").innerHTML = "EXPIRED";
   }
-}, 1000);
+}
+
+countdown();
+
+var x = setInterval(countdown, 1000);
